11b74ae2e2487c0f1a6317b1d990727d7134b57a
Fran Lozano <fjlozanos@gmail.com>
2022-07-12 16:32:14 +0200

    Orchestrate transformation steps

--- orchestration/data_manager/repository/jobs.py
+++ orchestration/data_manager/repository/jobs.py
@@ -1,16 +1,20 @@
-from dagster import job
+import os
+from dagster import job, file_relative_path
+from dagster_dbt import dbt_cli_resource, dbt_snapshot_op, dbt_build_op
 
 from ops import download_items_op_factory
 from resources.coches_net import coches_net_resource
 from resources.duckdb_parquet_io_manager import duckdb_parquet_io_manager
 
 
+DATABASE_FILE_NAME = 'coches.net.duckdb'
+TRANSFORMATION_DIR = os.path.abspath(file_relative_path(__file__, '../../../transformation'))
 DOCKER_CONFIG = {
     "resources": {
         "warehouse_io_manager": {
             "config": {
                 "download_dir": "/tmp/",
-                "duckdb_path": "/tmp/coches.net.duckdb",
+                "duckdb_path": f"/tmp/{DATABASE_FILE_NAME}",
             }
         }
     }
@@ -20,9 +24,9 @@ LOCAL_CONFIG = {
         "warehouse_io_manager": {
             "config": {
                 "download_dir": "/tmp/",
-                "duckdb_path": "../../data/coches.net.duckdb",
+                "duckdb_path": os.path.abspath(file_relative_path(__file__, f"../../data/{DATABASE_FILE_NAME}"))
             }
-        },
+        }
     }
 }
 
@@ -30,15 +34,22 @@ LOCAL_CONFIG = {
 @job(
     resource_defs={
         "warehouse_io_manager": duckdb_parquet_io_manager,
-        "coches_net_resource": coches_net_resource
+        "coches_net_resource": coches_net_resource,
+        "dbt":  dbt_cli_resource.configured({
+            "project_dir": TRANSFORMATION_DIR,
+            "profiles_dir": TRANSFORMATION_DIR
+        })
     }
 )
 def build_datasets_job():
     """
     Downloads all cars and motorbikes from the Coches.net API.
     """
+    # Extract and load data
     cars = download_items_op_factory(target_market="coches")()
     motorbikes = download_items_op_factory(target_market="motos")()
+    # Transform data
+    dbt_build_op(start_after=dbt_snapshot_op([cars, motorbikes]))
 
 
 if __name__ == "__main__":
