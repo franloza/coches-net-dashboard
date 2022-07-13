7e2025302510e607f43bcb9d9e4278fcdf419029
Jorjatorz <jorjatorz@gmail.com>
2022-07-13 23:11:25 +0200

    Changes to visualization

--- visualization/app.py
+++ visualization/app.py
@@ -1,7 +1,8 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc, Input, Output, State
+from calendar import c
+from dash import Dash, html, dcc, Input, Output, State, dash_table
 import plotly.express as px
 
 from model.data_provider import DataProvider
@@ -9,7 +10,7 @@ from model import figure_builder
 
 app = Dash(__name__)
 
-# Provider of the data from DB
+# Provider for the data from DB
 data_provider = DataProvider()
 
 
@@ -21,8 +22,15 @@ def left_panel():
             html.Div(
                 children=[
                     html.Label("KM"),
-                    dcc.Input(value=0, type="number", id="km-minumum", min=0),
+                    dcc.Input(value=0, type="number", id="km-minimum", min=0),
                     dcc.Input(value=100000, type="number", id="km-maximum", min=0),
+                ]
+            ),
+            html.Div(
+                children=[
+                    html.Label("Price"),
+                    dcc.Input(value=0, type="number", id="price-minimum", min=0),
+                    dcc.Input(value=100000, type="number", id="price-maximum", min=0),
                 ]
             ),
             dcc.Dropdown(
@@ -30,28 +38,41 @@ def left_panel():
             ),
             html.Label("Fuel Type"),
             dcc.Checklist(
-                {
-                    "Gasolina": "Gasolina",
-                    "Diesel": "Diesel",
-                    "Hibrido": "Hibrido",
-                    "plug-in hybrid": "Plug-in Hybrid",
-                    "Eléctrico": "Eléctrico",
-                },
-                ["Gasolina", "Diesel", "Hibrido", "plug-in hybrid", "Eléctrico"],
                 id="fuel-checklist",
             ),
             html.Label("Offer Type"),
             dcc.Checklist(
-                {
-                    "new": "New",
-                    "km0": "Km0",
-                    "used": "Used",
-                },
-                ["new", "km0", "used"],
                 id="offer-checklist",
             ),
+            html.Label("Main Province"),
+            dcc.Dropdown(id="province-dropdown", multi=True, searchable=True),
             html.Button("Search", id="search-button"),
         ],
+    )
+
+
+@app.callback(
+    Output(component_id="fuel-checklist", component_property="options"),
+    Output(component_id="fuel-checklist", component_property="value"),
+    Output(component_id="offer-checklist", component_property="options"),
+    Output(component_id="offer-checklist", component_property="value"),
+    Output(component_id="province-dropdown", component_property="options"),
+    Output(component_id="province-dropdown", component_property="value"),
+    Input(component_id="vehicle-dropdown", component_property="value"),
+)
+def populate_filters(vehicle):
+    # Common filters
+    fuel_checklist = data_provider.query_fuel_types(vehicle)
+    offer_checklist = data_provider.query_offer_types(vehicle)
+    province_dropdown = data_provider.query_provinces(vehicle)
+
+    return (
+        fuel_checklist,
+        fuel_checklist,
+        offer_checklist,
+        offer_checklist,
+        province_dropdown,
+        province_dropdown,
     )
 
 
@@ -75,28 +96,54 @@ def update_axis_componenet(vehicle):
 
 
 @app.callback(
-    Output(component_id="example-graph", component_property="figure"),
+    Output(component_id="data-graph", component_property="figure"),
+    Output(component_id="data-table", component_property="columns"),
+    Output(component_id="data-table", component_property="data"),
     Input(component_id="search-button", component_property="n_clicks"),
     Input(component_id="x-dropdown", component_property="value"),
     Input(component_id="y-dropdown", component_property="value"),
     Input(component_id="color-dropdown", component_property="value"),
     State(component_id="query-input", component_property="value"),
-    State(component_id="km-minumum", component_property="value"),
+    State(component_id="km-minimum", component_property="value"),
     State(component_id="km-maximum", component_property="value"),
+    State(component_id="price-minimum", component_property="value"),
+    State(component_id="price-maximum", component_property="value"),
     State(component_id="vehicle-dropdown", component_property="value"),
     State(component_id="fuel-checklist", component_property="value"),
     State(component_id="offer-checklist", component_property="value"),
+    State(component_id="province-dropdown", component_property="value"),
 )
-def update_output_div(_, x, y, color, title, km_min, km_max, vehicle, fuel, offer):
+def update_output_div(
+    _,
+    x,
+    y,
+    color,
+    title,
+    km_min,
+    km_max,
+    price_min,
+    price_max,
+    vehicle,
+    fuel,
+    offer,
+    provinces,
+):
     data = data_provider.query_data_by_parameters(
         title=title,
         km_min=km_min,
         km_max=km_max,
+        price_min=price_min,
+        price_max=price_max,
         vehicle=vehicle,
         fuel=fuel,
         offer=offer,
+        provinces=provinces,
     )
-    return figure_builder.build_price_distribution_figure(data, x=x, y=y, color=color)
+    return (
+        figure_builder.build_price_distribution_figure(data, x=x, y=y, color=color),
+        [{"name": i, "id": i} for i in data.columns],
+        data.to_dict("records"),
+    )
 
 
 app.layout = html.Div(
@@ -108,10 +155,29 @@ app.layout = html.Div(
             children=[
                 dcc.Loading(
                     children=[
-                        dcc.Dropdown(id="x-dropdown", value="price"),
-                        dcc.Dropdown(id="y-dropdown", value="km"),
-                        dcc.Dropdown(id="color-dropdown", value="year"),
-                        dcc.Graph(id="example-graph"),
+                        html.Div(
+                            style={"display": "inline"},
+                            children=[
+                                dcc.Dropdown(
+                                    id="x-dropdown",
+                                    value="price",
+                                ),
+                                dcc.Dropdown(
+                                    id="y-dropdown",
+                                    value="km",
+                                ),
+                                dcc.Dropdown(
+                                    id="color-dropdown",
+                                    value="year",
+                                ),
+                            ],
+                        ),
+                        dcc.Graph(id="data-graph"),
+                        dash_table.DataTable(
+                            id="data-table",
+                            page_current=0,
+                            page_size=10,
+                        ),
                     ],
                     type="circle",
                 ),
--- visualization/model/data_provider.py
+++ visualization/model/data_provider.py
@@ -16,23 +16,20 @@ class DataProvider:
         title: str = None,
         km_min: int = None,
         km_max: int = None,
+        price_min: int = None,
+        price_max: int = None,
         fuel: list = None,
         offer: list = None,
+        provinces: list = None,
     ) -> pd.DataFrame:
         query = f"""
             SELECT 
-                id,
-                creationDate::timestamp as creationDate, 
                 title,
-                'www.coches.net' || url as url, 
                 km::int as km,
                 year::int as year, 
                 cubicCapacity,
                 location_mainProvince as mainProvince, 
                 fuelType,
-                isFinanced::boolean as isFinanced,
-                isCertified::boolean as isCertified,
-                isProfessional,
                 publishedDate::timestamp as publishedDate,
                 price_amount as price,
                 FROM {vehicle} 
@@ -49,6 +46,11 @@ class DataProvider:
             query += f"km >= {km_min} AND "
         if km_max is not None:
             query += f"km <= {km_max} AND "
+        # Price
+        if price_min is not None:
+            query += f"price >= {price_min} AND "
+        if price_max is not None:
+            query += f"price <= {price_max} AND "
         # Fuel
         if fuel is not None and fuel:
             query += "("
@@ -58,8 +60,38 @@ class DataProvider:
             query += ") "
             query += "AND "
         # Offer
+        # Province
+        if provinces is not None and provinces:
+            query += "mainProvince IN ("
+            for elem in provinces:
+                query += f"'{elem}',"
+            query += ") AND "
 
         # To ease the filters append each one includes and "AND " at the end
         # Remove the last one from the query before execute
         query = query[:-5]
-        return self._con.execute(query).df()
+        return self._con.cursor().execute(query).df()
+
+    def query_fuel_types(self, vehicle: str):
+        query = f"""
+            SELECT 
+                DISTINCT(fuelType)
+                FROM {vehicle};
+            """
+        return [col[0] for col in self._con.cursor().execute(query).fetchall()]
+
+    def query_offer_types(self, vehicle: str):
+        query = f"""
+            SELECT 
+                DISTINCT(offerType_literal)
+                FROM {vehicle};
+            """
+        return [col[0] for col in self._con.cursor().execute(query).fetchall()]
+
+    def query_provinces(self, vehicle: str):
+        query = f"""
+            SELECT 
+                DISTINCT(mainProvince)
+                FROM {vehicle};
+            """
+        return [col[0] for col in self._con.cursor().execute(query).fetchall()]
